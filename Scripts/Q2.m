b5fb514ced3bf2148281eed9c14ddeb4e4d3fc57
Abdullah <iAbdullah.AlAmeri@gmail.com>
2019-09-11 10:33:13 -0600

    Added function that models the surface of airfoil

--- Scripts/Q2.m
+++ Scripts/Q2.m
@@ -11,7 +11,55 @@ c = 2; % chord length [m]
 alpha = 9; % AOA [Degrees]
 V_inf = 30; % Free-stream flow speed [m/s]
 roh_inf = 1.225; % Free-stream flow density [kg/m];
-P_inf = 101.3*10^3 ; % dynamic pressure [Pa];
+P_inf = 101.3*10^3 ; % free-stream pressure (statics)[Pa];
+
+
+%% Airfoil information: NACA 0012
+
+% integration the Coefficient of Axial force using equation 1.16 will
+% require you to know the change in height of the airfoil over the change
+% of the horizontal distance of the airfoil, therefore we need to define
+% the thickness or yt of the airfoil:
+
+
+% NACA Naming follows the following convention:
+
+% NACA MPXX
+%   example : NACA 2412
+%     M is the maximum camber divided by 100. In the example M=2 so the camber is 0.02 or 2% of the chord
+%     P is the position of the maximum camber divided by 10. In the example P=4 so the maximum camber is at 0.4 or 40% of the chord.
+%     XX is the thickness divided by 100. In the example XX=12 so the thiickness is 0.12 or 12% of the chord.
+
+
+% more info:
+
+% http://airfoiltools.com/airfoil/naca4digit
+
+
+% since it's NACA 0012 %thickness is t = 12/100
+
+% In our example, M = 0; Therfore, gradient change is 0.
+t = 20/100;
+
+% since our airfoil isn't cambered, we can say that from equations in the
+% website:
+
+% since it's symmetric airfoil, we will see that everything cancels and the
+% y location simply becomes 
+
+syms x
+yt = x (t/0.2).*c .* ( 0.2969.*sqrt(x./c) -0.1260*(x./c) - 0.3516.*(x./c).^2 + 0.2843.*(x./c).^3 - 0.1036.*(x./c).^4 ) ;
+
+
+Yu = yt;
+Yl =  -1.*(t/0.2).*c .* ( 0.2969.*sqrt(x./c) -0.1260*(x./c) - 0.3516.*(x./c).^2 + 0.2843.*(x./c).^3 - 0.1036.*(x./c).^4 ) ;
+
+
+% for Ca we would need to know how the lower and upper surface change
+% curvature, so we would need the derivative with respect to x.
+
+dYu_dx = diff(Yu);
+dYl_dx = diff(Yl);
 
 
 %%
@@ -37,8 +85,15 @@ Cp_lower = Spline.Cp_lower;
 
 numSeg = 100; % how many panels used to integrate?
 
+
 Cn = (1/c) * ( TrapezoidalRule(Cp_upper,0,c,numSeg) - TrapezoidalRule(Cp_lower,0,c,numSeg) ) ;
 
+% for Ca
+Ca = (1/c) * ( TrapezoidalRule(Cp_upper,0,c,numSeg) - TrapezoidalRule(Cp_lower,0,c,numSeg) ) ;
+
+
+Cl = Cn*cosd(alpha) - Ca*sind(alpha);
+Cd = Cn*sind(alpha) + Ca*cosd(alpha);
 
 %% 
 
